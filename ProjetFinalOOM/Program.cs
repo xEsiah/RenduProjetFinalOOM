262758fe87c07c7b88e45f87ca4beaa1d0357751
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-05-09 11:33:42 +0200

    Updating ClassDiagram, Creation of Protagonist/Serie/Season/Episode classes

--- ProjetFinalOOM/Program.cs
+++ ProjetFinalOOM/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public abstract class Protagonist{
+    public string Name { get; set; }
+    public string SourceSerie { get; set; }
+    public Protagonist(string name, string sourceSerie){
+        Name = name;
+        SourceSerie = sourceSerie;
+    }
+    public abstract void Interacting(Protagonist otherPortagonist);
+}
+
+public class Serie{
+    public string Title { get; set; }
+    public int BeginningYear { get; set; }
+    public string Gender { get; set; }
+    List<Season> Season; 
+    public Serie(string title, int beginningYear, string gender){
+        Title = title;
+        BeginningYear = beginningYear;
+        Gender = gender;
+    }
+}
+
+public class Season{
+    public int Number { get; set; }
+    List<Episode> Episode;
+}
+
+public class Episode{
+    public string Title;
+    public int DurationInMinutes;
+}
